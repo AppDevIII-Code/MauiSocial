900e63001d6482c532ca2e22dcbc8ca628d55b4d
Youmna <youmnab94@gmail.com>
2025-01-31 00:27:29 -0500

    Startup project

---
+++ MauiSocial/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiSocial
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiSocial/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiSocial
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
