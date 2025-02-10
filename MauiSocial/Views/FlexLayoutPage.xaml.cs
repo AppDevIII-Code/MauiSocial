85ebb646f8bd5dd67fb893d30025ddf25759f33a
Youmna Badawy <37409627+yobad@users.noreply.github.com>
2025-02-10 14:48:35 -0500

    layout binding2/3 done

--- MauiSocial/Views/FlexLayoutPage.xaml.cs
+++ MauiSocial/Views/FlexLayoutPage.xaml.cs
@@ -1,9 +1,33 @@
+using System.Collections.ObjectModel;
+
 namespace MauiSocial.Views;
 
 public partial class FlexLayoutPage : ContentPage
 {
+	string IMG_BASE_URI = "https://picsum.photos/";
+	string IMG_EXT = ".jpg";
+	int MIN_IMG_SIZE = 200;
+	int MAX_IMG_SIZE = 220;
+	Random rand = new Random();
+
+	public ObservableCollection<string> ImgUrls { get; set; }
 	public FlexLayoutPage()
 	{
 		InitializeComponent();
+		ImgUrls = new ObservableCollection<string>();
+		BindingContext = this;
+	}
+
+    private void Btn_AddImage_Clicked(object sender, EventArgs e)
+    {
+		var randImgUrl = GenerateRandomImgUrl();
+		ImgUrls.Add(randImgUrl);
+    }
+	private string GenerateRandomImgUrl()
+	{
+		int randomSize = rand.Next(MIN_IMG_SIZE, MAX_IMG_SIZE);
+		string imgUrl = $"{IMG_BASE_URI}/{randomSize}/{randomSize}{IMG_EXT}";
+
+		return imgUrl;
 	}
 }
--- MauiSocial/Views/PostPage.xaml.cs
+++ MauiSocial/Views/PostPage.xaml.cs
@@ -1,21 +1,55 @@
+using System.Collections.ObjectModel;
+
 namespace MauiSocial.Views;
 
 public partial class PostPage : ContentPage
 {
     int likes = 0;
+    public int Likes
+    {
+        get => likes;
+        set
+        {
+            if (likes != value)
+            {
+                likes = value;
+                OnPropertyChanged();
+            }
+
+        }
+    }
+    public ObservableCollection<string> Comments { get; set; } = new ObservableCollection<string>() { "Cool car", "haha", "lool" } ;
+    //List<string> comments = new List<string>() { "Cool car", "haha", "lool"};
+
+    //public List<string> Comments
+    //{
+    //    get => comments;
+    //    set
+    //    {
+    //        if (comments != value)
+    //        {
+    //            comments = value;
+    //            OnPropertyChanged();
+    //        }
+    //    }
+    //}
     public PostPage()
 	{
 		InitializeComponent();
+        BindingContext = this;
+
 	}
     private void Btn_Like_Clicked(object sender, EventArgs e)
     {
-        likes += 1;
-        LikesLabel.Text = $"{++likes} Likes";
+        Likes++;
+        //LikesLabel.Text = $"{++likes} Likes";
     }
 
     private async void Btn_Comment_Clicked(object sender, EventArgs e)
     {
-        await DisplayPromptAsync("Comment", "", "Send", "", "Add a comment");
+        var comment = await DisplayPromptAsync("Comment", "", "Send", "", "Add a comment");
+        Comments.Add(comment);
+
     }
 
     private async void Btn_Share_Clicked(object sender, EventArgs e)
