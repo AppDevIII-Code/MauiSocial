f7cd73115faa32b49b8a56a4cc77fe90a202f404
Youmna <youmnab94@gmail.com>
2025-03-13 22:49:48 -0400

    lab3 exo 1

--- MauiSocial/AppShell.xaml.cs
+++ MauiSocial/AppShell.xaml.cs
@@ -6,6 +6,21 @@
         {
             InitializeComponent();
         }
+        private void Btn_SwitchTheme_Clicked(object sender, EventArgs e)
+        {
+
+            switch (App.Current.UserAppTheme)
+            {
+                default:
+                case AppTheme.Light:
+                    App.Current.UserAppTheme = AppTheme.Dark;
+                    break;
+                case AppTheme.Dark:
+                    App.Current.UserAppTheme = AppTheme.Light;
+                    break;
+
+            }
+        }
 
 
     }
